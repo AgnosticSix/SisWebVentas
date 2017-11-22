041d17cbddd600c38a2bf3ab5b7bba4e8443f950
GSbeta <gsbetaww@gmail.com>
2017-11-22 09:45:06 -0600

    Confirmación

---
+++ SisWebVentas/Controllers/PersonasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SisWebVentas.Models;
+namespace SisWebVentas.Controllers
+{
+    public class PersonasController : Controller
+    {
+        // GET: Personas
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            PERSONAS personasModel = new PERSONAS();
+            return View(personasModel);
+        }
+    }
+}
---
+++ SisWebVentas/Controllers/ProductosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SisWebVentas.Models;
+namespace SisWebVentas.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            PRODUCTOS productosModel = new PRODUCTOS();
+            return View(productosModel);
+        }
+    }
+}
