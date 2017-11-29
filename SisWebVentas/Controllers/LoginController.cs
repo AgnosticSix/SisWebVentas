32458926e7739ff347611dfb298351330df7f8f6
AgnosticSix <mateo_godfather@hotmail.com>
2017-11-28 21:02:44 -0600

    Login and Logout actions added

---
+++ SisWebVentas/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using SisWebVentas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SisWebVentas.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autorizar(SisWebVentas.Models.USUARIOS userModel)
+        {
+            using (SisWebVentasEntities1 db = new SisWebVentasEntities1())
+            {
+                var userDetails = db.USUARIOS.Where(x => x.USUARIO == userModel.USUARIO && x.CONTRASENA == userModel.CONTRASENA).FirstOrDefault();
+                if(userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Usuario o contraseña incorrectos";
+                    return View("Login", userModel);
+                }
+                else
+                {
+                    Session["cve_usuario"] = userDetails.CVE_USUARIO;
+                    return RedirectToAction("Login", "Index");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
--- SisWebVentas/Models/USUARIOS.cs
+++ SisWebVentas/Models/USUARIOS.cs
@@ -11,7 +11,9 @@ namespace SisWebVentas.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class USUARIOS
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,7 +23,12 @@ namespace SisWebVentas.Models
         }
     
         public int CVE_USUARIO { get; set; }
+        [DisplayName("Nombre de Usuario")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string USUARIO { get; set; }
+        [DisplayName("Contraseña")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.Password)]
         public string CONTRASENA { get; set; }
         public Nullable<bool> ACTIVO { get; set; }
         public Nullable<bool> ADMINISTRADOR { get; set; }
@@ -29,5 +36,6 @@ namespace SisWebVentas.Models
         public virtual PERSONAS PERSONAS { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<VENTAS> VENTAS { get; set; }
+        public string LoginErrorMessage { get; internal set; }
     }
 }
